d17fa63dece2a3c46abb7c7c4310bd2c158852a3
plocket <52798256+plocket@users.noreply.github.com>
2020-10-16 22:06:33 -0400

    Try destination_repo and INPUT

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone --single-branch --branch "$TARGET_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$REPO_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch "$TARGET_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 ls -la "$CLONE_DIR"
 
 #echo "Cleaning destination repository of old files"
